7759991fa05b72981b2bcb13a32d5fcf580d9c0f
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-08-18 16:23:38 +0100

    theme

--- src/main.js
+++ src/main.js
@@ -1,21 +1,66 @@
 import { EditorView, basicSetup } from "codemirror";
-import { oneDark } from "@codemirror/theme-one-dark";
+import { githubDark } from "@fsegurai/codemirror-theme-github-dark";
 import Split from "split.js";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
+
+import "@xterm/xterm/css/xterm.css";
 import "./style.css";
 
 document.querySelector("#app").innerHTML = `
-<div class="split size-full">
-    <div id="split-editor" class=""></div>
-    <div id="split-output" class="bg-green-700"></div>
-</div>
+  <div id="container" class="h-full w-full flex flex-col">
+    <div class="flex items-center gap-2 bg-[#1e1e1e] text-gray-200 px-2 py-1 text-sm">
+      <button id="runBtn" class="px-2 py-0.5 rounded hover:bg-gray-700">▶ Run</button>
+      <button id="shareBtn" class="px-2 py-0.5 rounded hover:bg-gray-700">Share</button>
+    </div>
+
+    <div class="h-full w-full flex-1">
+      <div id="split-editor"></div>
+      <div id="split-output" class="bg-[#1e1e1e]"></div>
+    </div>
+  </div>
 `;
 
 Split(["#split-editor", "#split-output"], {
   direction: "vertical",
+  sizes: [70, 30],
+  minSize: [100, 100],
+  cursor: "row-resize",
 });
 
-new EditorView({
+const editor = new EditorView({
   doc: "printf('Hello, from web ide buddy!');",
-  extensions: [basicSetup, oneDark],
+  extensions: [basicSetup, githubDark],
   parent: document.getElementById("split-editor"),
 });
+
+const terminal = new Terminal({
+  theme: {
+    background: "#1e1e1e",
+    foreground: "#ffffff",
+    cursor: "#ffffff",
+  },
+  fontSize: 14,
+  fontFamily: 'Monaco, Menlo, "Ubuntu Mono", monospace',
+});
+
+let xtermContainer = document.getElementById("split-output");
+
+const fitAddon = new FitAddon();
+terminal.loadAddon(fitAddon);
+
+const observer = new ResizeObserver((_) => fitAddon.fit());
+observer.observe(xtermContainer);
+
+terminal.open(xtermContainer);
+
+terminal.write("Welcome to your Web IDE 🚀\r\n$ ");
+
+document.getElementById("runBtn").addEventListener("click", () => {
+  terminal.clear();
+
+  const code = editor.state.doc.toString();
+  terminal.write(`\r\nRunning code...\r\n`);
+});
+
+document.getElementById("shareBtn").addEventListener("click", () => {});
